bb83a872a573996316efc027f669cd182c5bc9f6
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-14 14:28:11 +0600

    installed codemiorror set an iframe and created an js file named Editor in the component folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Editor from "./components/Editor";
+
 function App() {
-	return "hello world";
+	return (
+		<>
+			<div className="pane top-pane">
+				<Editor />
+				<Editor />
+				<Editor />
+			</div>
+			<div className="pane">
+				<iframe
+					title="output"
+					sendbox="allow-script"
+					style={{ border: "none" }}
+					width="100%"
+					height="100%"
+				/>
+			</div>
+		</>
+	);
 }
 
 export default App;
